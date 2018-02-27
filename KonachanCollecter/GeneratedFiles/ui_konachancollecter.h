854c5fd64eac206b7dadd39ab3afc3f5d99c8481
88424 <88424@MSI>
2018-02-27 15:09:56 +0800

    初版，需要重构

---
+++ KonachanCollecter/GeneratedFiles/ui_konachancollecter.h
@@ -0,0 +1,120 @@
+/********************************************************************************
+** Form generated from reading UI file 'konachancollecter.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_KONACHANCOLLECTER_H
+#define UI_KONACHANCOLLECTER_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_KonachanCollecterClass
+{
+public:
+    QWidget *centralWidget;
+    QLabel *label;
+    QLabel *label_2;
+    QLineEdit *lineEdit_tags;
+    QLineEdit *lineEdit_folderPath;
+    QPushButton *pushButton_selectFolder;
+    QPushButton *pushButton_start;
+    QProgressBar *progressBar;
+    QLabel *label_progress;
+    QPushButton *pushButton_tags;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *KonachanCollecterClass)
+    {
+        if (KonachanCollecterClass->objectName().isEmpty())
+            KonachanCollecterClass->setObjectName(QStringLiteral("KonachanCollecterClass"));
+        KonachanCollecterClass->resize(929, 292);
+        centralWidget = new QWidget(KonachanCollecterClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(100, 120, 61, 16));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(100, 160, 61, 16));
+        lineEdit_tags = new QLineEdit(centralWidget);
+        lineEdit_tags->setObjectName(QStringLiteral("lineEdit_tags"));
+        lineEdit_tags->setGeometry(QRect(180, 120, 451, 21));
+        lineEdit_folderPath = new QLineEdit(centralWidget);
+        lineEdit_folderPath->setObjectName(QStringLiteral("lineEdit_folderPath"));
+        lineEdit_folderPath->setGeometry(QRect(180, 160, 451, 21));
+        pushButton_selectFolder = new QPushButton(centralWidget);
+        pushButton_selectFolder->setObjectName(QStringLiteral("pushButton_selectFolder"));
+        pushButton_selectFolder->setGeometry(QRect(650, 160, 92, 28));
+        pushButton_start = new QPushButton(centralWidget);
+        pushButton_start->setObjectName(QStringLiteral("pushButton_start"));
+        pushButton_start->setGeometry(QRect(760, 120, 91, 71));
+        progressBar = new QProgressBar(centralWidget);
+        progressBar->setObjectName(QStringLiteral("progressBar"));
+        progressBar->setEnabled(true);
+        progressBar->setGeometry(QRect(180, 80, 451, 23));
+        progressBar->setValue(24);
+        progressBar->setTextVisible(true);
+        label_progress = new QLabel(centralWidget);
+        label_progress->setObjectName(QStringLiteral("label_progress"));
+        label_progress->setGeometry(QRect(180, 50, 241, 16));
+        pushButton_tags = new QPushButton(centralWidget);
+        pushButton_tags->setObjectName(QStringLiteral("pushButton_tags"));
+        pushButton_tags->setGeometry(QRect(650, 120, 92, 28));
+        KonachanCollecterClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(KonachanCollecterClass);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 929, 26));
+        KonachanCollecterClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(KonachanCollecterClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        KonachanCollecterClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(KonachanCollecterClass);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        KonachanCollecterClass->setStatusBar(statusBar);
+
+        retranslateUi(KonachanCollecterClass);
+
+        QMetaObject::connectSlotsByName(KonachanCollecterClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *KonachanCollecterClass)
+    {
+        KonachanCollecterClass->setWindowTitle(QApplication::translate("KonachanCollecterClass", "KonachanCollecter", 0));
+        label->setText(QApplication::translate("KonachanCollecterClass", "Tag(s):", 0));
+        label_2->setText(QApplication::translate("KonachanCollecterClass", "Folder:", 0));
+        lineEdit_tags->setText(QString());
+        pushButton_selectFolder->setText(QApplication::translate("KonachanCollecterClass", "Select", 0));
+        pushButton_start->setText(QApplication::translate("KonachanCollecterClass", "Start", 0));
+        label_progress->setText(QApplication::translate("KonachanCollecterClass", "Please wait for a minute...", 0));
+        pushButton_tags->setText(QApplication::translate("KonachanCollecterClass", "All tags", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class KonachanCollecterClass: public Ui_KonachanCollecterClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_KONACHANCOLLECTER_H
--- KonachanCollecter/konachancollecter.cpp
+++ KonachanCollecter/konachancollecter.cpp
@@ -4,9 +4,158 @@ KonachanCollecter::KonachanCollecter(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	// 隐藏进度
+	ui.label_progress->hide();
+	ui.progressBar->hide();
+	// 提示信息
+	ui.lineEdit_tags->setPlaceholderText("6 tags at most:dress+long_hair+...");
+	ui.lineEdit_folderPath->setPlaceholderText("All pictures will be downloaded to this folder.");
+	// 信号槽
+	connect(ui.pushButton_selectFolder, SIGNAL(clicked()), this, SLOT(getFolderPath()));
+	connect(ui.pushButton_start, SIGNAL(clicked()), this, SLOT(sendRequest()));
+	// 初始化SSL
+	sslConfig.setPeerVerifyMode(QSslSocket::VerifyNone);
+	sslConfig.setProtocol(QSsl::TlsV1_2);
+	request.setSslConfiguration(sslConfig);
 }
 
 KonachanCollecter::~KonachanCollecter()
 {
 
+}
+
+// 成员初始化
+void KonachanCollecter::memberInit()
+{
+	imageCount = 0;
+	downloadCount = 0;
+	imageInfoUrl.clear();
+}
+
+// 获取保存图片的目标文件夹路径
+void KonachanCollecter::getFolderPath()
+{
+	folderPath = QFileDialog::getExistingDirectory(nullptr, "Please select the destination folder...","D:/Picture/tmp");
+	ui.lineEdit_folderPath->setText(folderPath);
+}
+
+// 请求图片列表
+void KonachanCollecter::sendRequest()
+{
+	if ("stop" == ui.pushButton_start->text())
+	{
+		// 结束所有https线程
+	}
+	// 界面处理
+	ui.label_progress->show();
+	ui.pushButton_start->setText("Stop");
+	// 发送请求
+	memberInit();
+	QNetworkAccessManager* pManager = new QNetworkAccessManager();
+	connect(pManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getInfoAddr(QNetworkReply*)));
+	requestUrl = "https://konachan.com/post?tags=" + ui.lineEdit_tags->text();
+	request.setUrl(requestUrl);
+	pManager->get(request);
+}
+
+// 请求图片概览
+void KonachanCollecter::getInfoAddr(QNetworkReply* reply)
+{
+	QString listPage = reply->readAll();
+
+	int pageCount = 0;
+	QString tmp;
+	int pos = 0;
+	// 获取图片列表页数
+	QRegExp rx("a href=\"/post\\?page=\\d+");
+	rx.lastIndexIn(listPage);
+	tmp = rx.cap();
+	rx.setPattern("\\d+");
+	rx.indexIn(tmp);
+	pageCount = rx.cap().toInt();
+	// 获取第一页所有图片的概览地址
+	rx.setPattern("/post/show/\\d+/[0-9a-z-_]+");
+	while ((pos = rx.indexIn(listPage, pos)) != -1)
+	{
+		imageInfoUrl<<("https://konachan.com" + rx.cap());
+		pos += rx.matchedLength();
+	}
+	// 获取2-last页所有图片的概览地址
+	QEventLoop loop;
+	QNetworkAccessManager* pManager = new QNetworkAccessManager();
+	for (;pageCount > 1;pageCount--)
+	{
+		disconnect(reply);
+		requestUrl = QString("https://konachan.com/post?page=%1&tags=").arg(pageCount) + ui.lineEdit_tags->text();
+		request.setUrl(requestUrl);
+		reply = pManager->get(request);
+		connect(reply, SIGNAL(finished()), &loop, SLOT(quit()));
+		loop.exec();
+		listPage = reply->readAll();
+		pos = 0;
+		while ((pos = rx.indexIn(listPage, pos)) != -1)
+		{
+			imageInfoUrl << ("https://konachan.com" + rx.cap());
+			pos += rx.matchedLength();
+		}
+	}
+	imageCount = imageInfoUrl.count();// 图片总数
+	reply->deleteLater();
+	// 显示进度条
+	ui.label_progress->setText(QString("Downloading...no.1/%1").arg(imageCount));
+	ui.progressBar->setRange(0, imageCount);
+	ui.progressBar->setValue(0);
+	ui.progressBar->show();
+	// 请求所有图片的概览页面
+	connect(pManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getDownloadAddr(QNetworkReply*)));
+	for (auto iter : imageInfoUrl)
+	{
+		qDebug() << iter;
+		request.setUrl(iter);
+		pManager->get(request);
+	}
+}
+
+// 请求原图下载：png version->jpg version
+void KonachanCollecter::getDownloadAddr(QNetworkReply* reply)
+{
+	QString infoPage = reply->readAll();
+	reply->deleteLater();
+	QRegExp rx("https://konachan.com/image/[0-9a-z]+/Konachan.com[0-9a-z-_%]+.png|https://konachan.com/image/[0-9a-z]+/Konachan.com[0-9a-z-_%]+.jpg");
+	QNetworkAccessManager* pManager = new QNetworkAccessManager();
+	rx.indexIn(infoPage);
+	requestUrl = rx.cap();
+	request.setUrl(requestUrl);
+	connect(pManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(downloadImage(QNetworkReply*)));
+	pManager->get(request);
+}
+
+// 下载器
+void KonachanCollecter::downloadImage(QNetworkReply* reply)
+{
+	QRegExp rx("Konachan.com.*");
+	QString str = reply->url().toString();
+	rx.indexIn(str);
+	QString fileName = rx.cap();
+	QFile file(folderPath+"/"+fileName);
+	if (!file.open(QIODevice::WriteOnly))
+	{
+
+	}
+	file.write(reply->readAll());
+	file.close();
+	reply->deleteLater();
+	downloadCount++;
+	if (downloadCount != imageCount)
+	{
+		ui.label_progress->setText(QString("Downloading...no.%1/%2").arg(downloadCount + 1).arg(imageCount));
+		ui.progressBar->setValue(downloadCount);
+		qDebug() << "filename:" << fileName;
+	}
+	else
+	{
+		ui.label_progress->hide();
+		ui.progressBar->hide();
+		QMessageBox::about(nullptr, "Finished!", QString("All images have been downloaded!(%1/%2)").arg(downloadCount).arg(imageCount));
+	}
 }
